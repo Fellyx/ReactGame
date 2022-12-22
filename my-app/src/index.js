407800eb632586abf52aad8bbe009567d12924bd
Fellyx <Fellyx@users.noreply.github.com>
2022-12-22 12:23:10 -0300

    Moving the handleClick method to the Game. The handleClick method was moved from the board to the game component, creating the concatenating for the history.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,21 +23,7 @@ function Square(props){
   );
 }
 
-class Board extends React.Component {
-  handleClick(i){
-    const squares = this.state.squares.slice();
-
-    if (calculateWinner(squares) || squares[i]){
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-  
+class Board extends React.Component {  
   renderSquare(i) {
     return (
       <Square 
@@ -79,6 +65,24 @@ class Game extends React.Component {
       }],
       xIsNext: true,
     };
+  }
+
+  handleClick(i){
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
